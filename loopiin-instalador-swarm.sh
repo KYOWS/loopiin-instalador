8add89ddf6f1cd2134f7aa5e32894a52ae6485a7
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-21 11:49:48 -0300

    Update loopiin-instalador-swarm.sh

--- loopiin-instalador-swarm.sh
+++ loopiin-instalador-swarm.sh
@@ -594,9 +594,9 @@ services:
       - /var/run/docker.sock:/var/run/docker.sock
       - /var/lib/docker/volumes:/var/lib/docker/volumes
       #- /docker/portainer/data:/data
-      - /docker/portainer/certs/ca.pem:/certs/ca.pem:ro
-      - /docker/portainer/certs/agent.pem:/certs/cert.pem:ro
-      - /docker/portainer/certs/agent.key:/certs/key.pem:ro
+      #- /docker/portainer/certs/ca.pem:/certs/ca.pem:ro
+      #- /docker/portainer/certs/agent.pem:/certs/cert.pem:ro
+      #- /docker/portainer/certs/agent.key:/certs/key.pem:ro
     labels:
       - "traefik.enable=false" 
     networks:
@@ -616,12 +616,12 @@ services:
     command:      
       - -H
       - tcp://tasks.agent:9001
-      - --tlsverify
-      - --tlscacert=/certs/ca.pem
+      #- --tlsverify
+      #- --tlscacert=/certs/ca.pem
     volumes:
       - portainer_data:/data
       #- /docker/portainer/data:/data
-      - /docker/portainer/certs/ca.pem:/certs/ca.pem:ro
+      #- /docker/portainer/certs/ca.pem:/certs/ca.pem:ro
     networks:
       - agent_network
       - web
