08506346e022a792416546c6730189129901b558
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-21 17:25:30 -0300

    Update loopiin-instalador-swarm.sh

--- loopiin-instalador-swarm.sh
+++ loopiin-instalador-swarm.sh
@@ -775,17 +775,7 @@ EOL
   [tls.options.default]
     sslStrategies = ["tls.SniStrict"]
     minVersion = "VersionTLS12"
-    maxVersion = "VersionTLS13"
-    cipherSuites = [
-      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-      "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305",
-      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-      "TLS_RSA_WITH_AES_256_GCM_SHA384",
-      "TLS_RSA_WITH_AES_128_GCM_SHA256",
-      "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-      "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305",
-      "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
-    ]
+    maxVersion = "VersionTLS13"    
     curvePreferences = [
       "X25519",
       "secp521r1",
