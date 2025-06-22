c8f4930f76bf5e2b86c46efec2fdddeb200b3563
Wallison Santos <110735811+KYOWS@users.noreply.github.com>
2025-06-22 12:22:07 -0300

    Update loopiin-instalador-swarm.sh

--- loopiin-instalador-swarm.sh
+++ loopiin-instalador-swarm.sh
@@ -80,7 +80,7 @@ echo -e "${YELLOW}Liberando portas essenciais...${NC}"
 
     echo -e "${GREEN}✅ Firewall configurado e ativo.${NC}"
     # Apenas para mostrar um output mais limpo e focado
-    (sudo ufw status | head -n 1 && sudo ufw status | grep -E '80|22|443|2377|7946|4789|9001') || sudo ufw status
+    (sudo ufw status | head -n 1 && sudo ufw status | grep -E '80|22|443|2377|7946|4789') || sudo ufw status
     return 0
 }
 
